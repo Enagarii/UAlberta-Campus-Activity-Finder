c806a3c5aaa3eaae750abd78b730cff6e55699c0
Vin <vlou1@nait.ca>
2025-02-15 16:17:05 -0700

    Currently writing into a .json file.

--- src/add_event.py
+++ src/add_event.py
@@ -2,5 +2,5 @@ import json
 
 def dumpEventToJSON(event, json_file):
     write_file = open(json_file, 'w')
-    # write_file.write(json.dumps(event))
+    write_file.write(json.dumps(event))
     write_file.close()
