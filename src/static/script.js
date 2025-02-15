69ec75a300e89ca58f82c296f67b3a40946dd73c
gurmeharbb <gurmehar.bhatti@hotmail.com>
2025-02-15 14:44:35 -0700

    modified:   src/static/script.js

--- src/static/script.js
+++ src/static/script.js
@@ -1,6 +1,12 @@
 const body = document.querySelector("body");
 body.setAttribute("style", "margin: 0px; background: rgb(200, 255, 203)")
 
+// This dynamically creates a <link> element to load the "Roboto Slab" font.
+const fontLink = document.createElement("link");
+fontLink.href = "https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@600&display=swap";
+fontLink.rel = "stylesheet";
+document.head.appendChild(fontLink);
+
 // Left bar to show the contents of the activities
 const sidebar = document.createElement("div");
 sidebar.setAttribute("style", 
@@ -29,7 +35,7 @@ sidebar.appendChild(tabContainer);
 // Title Bar
 const banner = document.createElement("div");
 banner.setAttribute("style", 
-    "width: 100%; height: 100px; margin-bottom: 10px; background-color:rgb(127, 235, 127); box-shadow: 0px 3px 3px rgb(133, 133, 133); color: rgb(255, 255, 255); font-weight: bold; font-size: 48px; font-family: Georgia; display: flex; align-items: center; justify-content: center; margin-top: 0px")
+    "width: 100%; height: 100px; margin-bottom: 10px; background-color:rgb(39, 93, 56); box-shadow: 0px 3px 3px rgb(133, 133, 133); color: rgb(242, 205, 0); font-weight: 600; font-size: 60px; font-family: 'Roboto Slab', serif; display: flex; align-items: center; justify-content: center; margin-top: 0px")
 banner.textContent = "Campus Activity Finder"
 body.appendChild(banner);
 
